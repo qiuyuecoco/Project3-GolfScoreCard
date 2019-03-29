87abb07eb0b62969e07cd660b5db152f577f410a
Keisha Ding <keisha.seow@outlook.com>
2019-03-28 23:04:19 -0600

    input options displaying correct data

---
+++ model.js
@@ -0,0 +1,12 @@
+class Players{
+    constructor(){
+        this.name = name;
+        this.points = points;
+    }
+}
+class Course {
+
+}
+class Hole{
+
+}
--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let allCourse;
-let individualCourses;
+let aCourse;
 
 //select box for selected Course
 function allCourses() {
@@ -18,23 +18,52 @@ function allCourses() {
     xhttp.send();
 }
 //individual Course information
-function individualCourse() {
+// img extra: <img class="courseImg ${aCourse.courses[i].id}" src="aCourse.courses[i].image"/>
+function individualCourse(courseID) {
+    $(".container").html("");
+    $(".selectTee").html("");
+    $(".selectTee").append(`<option>Select difficulty: </option>`);
+
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState === 4 && this.status === 200){
-            individualCourses = JSON.parse(this.responseText);
-            console.log(individualCourses);
-            for (let i = 0; i < individualCourses.length; i++) {
-                $(".table").append(`individualCourses[i]`);
+            aCourse = JSON.parse(this.responseText);
+            console.log(aCourse);
+            for (let i = 0; i < aCourse.data.holes.length; i++) {
+                $(".container").append(`<div class="courseContainer ${aCourse.data.holes[i].hole}">
+                ${aCourse.data.holes[i].hole}</div>`);
+            }
+
+            for (let i = 0; i < aCourse.data.holes[0].teeBoxes.length; i++) {
+                if (aCourse.data.holes[0].teeBoxes[i].teeType != "auto change location") {
+                    $(".selectTee").append(`<option value="${aCourse.data.holes[0].teeBoxes[i].teeType}">
+            ${aCourse.data.holes[0].teeBoxes[i].teeType}</option>`);
+                }
+            }
+        }
+    };
+    xhttp.open("GET", `https://golf-courses-api.herokuapp.com/courses/${courseID}`, true);
+    xhttp.send();
+}
+function teeType(teeType) {
+    $("");
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState === 4 && this.status === 200){
+            aCourse = JSON.parse(this.responseText);
+            console.log(aCourse);
+            for (let i = 0; i < aCourse.data.holes.length; i++) {
+                $(".container").append(`<div class="courseContainer ${aCourse.data.holes[i].hole}">
+                ${aCourse.data.holes[i].hole}
+                </div>`);
 
             }
         }
     };
-
-    xhttp.open("GET", "https://golf-courses-api.herokuapp.com/courses/${courseid}", true);
+    xhttp.open("GET", `https://golf-courses-api.herokuapp.com/courses/${courseID}`, true);
     xhttp.send();
-}
 
+}
 // function loadCourse(event) {
 //     constructor()
 // }
@@ -42,3 +71,9 @@ function individualCourse() {
 
 //select > onChange > this.value
 // https://uxcobra.com/golfapi/courses/18300
+function modal() {
+    $(".modal").show();
+}
+function closeModal(element) {
+    $(element).parent().parent().parent().fadeOut();
+}
